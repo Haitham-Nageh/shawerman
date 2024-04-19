37c3a8cd13ed7833f6b03fb0963b4ef47772c46a
Haitham-Nageh <haithamalawneh13@gmail.com>
2024-04-19 21:06:57 +0200

    first one

---
+++ main.js
@@ -0,0 +1,28 @@
+const filterBtns = [...document.querySelectorAll(".filter button")];
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // remove class 'active' from all buttons
+    filterBtns.forEach((el) => {
+      el.classList.remove("active");
+    });
+    // add class 'active' to
+    btn.classList.add("active");
+    const dataSearch = btn.dataset.search;
+    const containers = document.querySelectorAll(".imgcontainer");
+    if (["all", undefined].includes(dataSearch))
+      containers.forEach((cont) => {
+        cont.style.display = "block";
+      });
+      else if(dataSearch==="none")
+      containers.forEach((cont) => {
+        cont.style.display = "none";
+      });
+    else
+      containers.forEach((cont) => {
+        if ([...cont.classList].includes(dataSearch))
+          cont.style.display = "block";
+        else cont.style.display = "none";
+      });
+  });
+});
